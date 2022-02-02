00f2da896358ce1fe924df6fe4252ea1c6db07ea
Aswanth Ds <85214585+Aswanthds@users.noreply.github.com>
2022-02-02 09:37:54 +0530

    Update send-email.js

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,5 +1,23 @@
-<script>
+function sendEmail() {
 
+	Email.send({	Host: "smtp.gmail.com",
 
+	Username : "musicaswanth@gmail.com",
 
-</script>
+	Password : "musicaswanth1452@2005",
+
+	To :  "worldofaswanth@gmail.com",
+
+	From : document.getElementById("email").value,
+
+	Subject : "New Contact Form",
+
+	Body : "<email body>",
+
+	}).then(
+
+		message => alert("mail sent successfully")
+
+	);
+
+}
